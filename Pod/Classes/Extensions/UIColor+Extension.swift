4ca7675092a6f8352c3a404396e949cde9180687
Alex Bechmann <alex_bechmann@hotmail.com>
2015-05-31 17:12:37 +0200

    uicolor hex

--- Pod/Classes/Extensions/UIColor+Extension.swift
+++ Pod/Classes/Extensions/UIColor+Extension.swift
@@ -17,4 +17,12 @@ extension UIColor{
         var randomBlue:CGFloat = CGFloat(drand48())
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
+    
+    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+        
+        let r = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((hex & 0x00FF00) >> 08) / 255.0
+        let b = CGFloat((hex & 0x0000FF) >> 00) / 255.0
+        self.init(red:r, green:g, blue:b, alpha:alpha)
+    }
 }
