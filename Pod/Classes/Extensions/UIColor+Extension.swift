8c05bb27f6a70cf3d9b7e92d5adb0d48a7ddc725
Alex Bechmann <alex_bechmann@hotmail.com>
2015-05-31 17:12:50 +0200

    public

--- Pod/Classes/Extensions/UIColor+Extension.swift
+++ Pod/Classes/Extensions/UIColor+Extension.swift
@@ -18,7 +18,7 @@ extension UIColor{
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
     
-    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+    public convenience init(hex: Int, alpha: CGFloat = 1.0) {
         
         let r = CGFloat((hex & 0xFF0000) >> 16) / 255.0
         let g = CGFloat((hex & 0x00FF00) >> 08) / 255.0
