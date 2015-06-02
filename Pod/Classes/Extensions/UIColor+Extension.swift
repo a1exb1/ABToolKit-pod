d2bf3337c104c0f4a0263dc1b4ffb5774199616f
Alex Bechmann <alex_bechmann@hotmail.com>
2015-06-02 18:36:27 +0200

    new hex bicolour

--- Pod/Classes/Extensions/UIColor+Extension.swift
+++ Pod/Classes/Extensions/UIColor+Extension.swift
@@ -18,20 +18,14 @@ public extension UIColor {
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
     
-    public convenience init(hex: Int, alpha: CGFloat = 1.0) {
-        
-        let r = CGFloat((hex & 0xFF0000) >> 16) / 255.0
-        let g = CGFloat((hex & 0x00FF00) >> 08) / 255.0
-        let b = CGFloat((hex & 0x0000FF) >> 00) / 255.0
-        self.init(red:r, green:g, blue:b, alpha:alpha)
-    }
-    
     public convenience init(hex: Int) {
         
-        let alpha: CGFloat = 1.0
-        let r = CGFloat((hex & 0xFF0000) >> 16) / 255.0
-        let g = CGFloat((hex & 0x00FF00) >> 08) / 255.0
-        let b = CGFloat((hex & 0x0000FF) >> 00) / 255.0
-        self.init(red:r, green:g, blue:b, alpha:alpha)
+        let components = (
+            R: CGFloat((hex >> 16) & 0xff) / 255,
+            G: CGFloat((hex >> 08) & 0xff) / 255,
+            B: CGFloat((hex >> 00) & 0xff) / 255
+        )
+        
+        self.init(red: components.R, green: components.G, blue: components.B, alpha: 1)
     }
 }
